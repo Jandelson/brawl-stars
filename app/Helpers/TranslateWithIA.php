a5c7706eae9390a642bf7baf3bb2da50fdaa672e
Jandelson Oliveira <jandelson_oliveira@yahoo.com.br>
2025-01-05 10:59:42 -0300

    Add try catch

--- app/Helpers/TranslateWithIA.php
+++ app/Helpers/TranslateWithIA.php
@@ -2,33 +2,41 @@
 
 namespace App\Helpers;
 
-use App\Enums\LanguageIAEnum;
+use Exception;
 use Illuminate\Support\Facades\Http;
 
 class TranslateWithIA
 {
     public static function translate(string $text, string $language): string
     {
-        $data = [
-            'contents' => [
-                'parts' => [
-                    ['text' => "translate text to $language text: '$text'"]
+        try {
+            $translate = '';
+
+            $data = [
+                'contents' => [
+                    'parts' => [
+                        ['text' => "translate text to $language text: '$text'"]
+                    ]
                 ]
-            ]
-        ];
+            ];
 
-        $response = Http::withHeaders([
-            'Content-Type' => 'application/json',
-        ])->post(
-            'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key='. env('GEMINI_API_KEY'),
-            $data
-        );
+            
+            $response = Http::withHeaders([
+                'Content-Type' => 'application/json',
+            ])->post(
+                'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key='. env('GEMINI_API_KEY'),
+                $data
+            );
 
-        if ($response->successful()) {
-            $responseData = $response->json();
-            return $responseData['candidates'][0]['content']['parts'][0]['text'];
-        } else {
-            throw new \Exception('Translation request failed');
+            if ($response->successful()) {
+                $responseData = $response->json();
+                $translate = $responseData['candidates'][0]['content']['parts'][0]['text'];
+            }
+
+            return $translate;
+
+        } catch (Exception $error) {
+            throw new \Exception('Translation request failed: ' . $error->getMessage());
         }
     }
 }
